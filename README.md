1af2c811e8ce689e295db4507afddfef65d9a4a3
Pattaravut Maleehuan <pattaravut.m@gmail.com>
2022-12-17 19:53:52 +0900

    Update

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # pwa-profile
 
-Offline Profile page
-Hey
+Ongoing work for building a profile web page that can work without an internet connection.
+
+## Intention
+I also want to learn about how to make an offline web app. This is inspired by Pokedex.org. 
+In this work, I want to simply use `React` and `Typescript` to make an offline experience quicker. 
+Since I don't have any experience in building a website, this work could take more time...
--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -24,9 +24,7 @@ type Config = {
 };
 
 export function register(config?: Config) {
-  console.log(process.env.NODE_ENV)
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    console.log('production?');
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {\
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     console.log(publicUrl);
